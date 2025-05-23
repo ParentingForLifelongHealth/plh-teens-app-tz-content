1a2d21f2e488c4b9b980675126177312e5de9447
EsmeeIDEMS <esmee@idems.international>
2025-05-23 16:03:33 +0100

    content: 1.2.1

--- config.ts
+++ config.ts
@@ -4,6 +4,16 @@ import { loadEncryptedConfig } from "scripts";
 /** TZ config extends the default config **/
 const config = extendDeploymentConfig({ name: "plh_teens_tz", parent: "plh_teens" });
 
+config.google_drive = {
+  sheets_folder_ids: [
+    "1_r77FxTtub64tbHL1EgF2CZDkQShxaES", // Scale Up
+    "1e8-inTLEHkdskV3OAtes7QNsiL7Rwr5v", // Facebook Ad Campaign
+    "1UXVz71HniwdtklFnGUEBzzj8ZHI9oQVo" // Library field values
+  ],
+  assets_folder_ids: ["1bT13rBBqxkzAw-kl_T5rbijT6rysLfLB"], // Scale Up
+};
+
+
 config.app_data!.sheets_filter_function = (flow) =>
   !["debug", "component_demo", "example_hardcoded", "campaign_rows_debug"].includes(
     flow.flow_subtype!
@@ -16,7 +26,7 @@ config.translations.translated_strings_path = "./app_data/translations_source/tr
 
 config.git = {
   content_repo: "https://github.com/IDEMSInternational/plh-teens-app-tz-content.git",
-  content_tag_latest: "1.2.0",
+  content_tag_latest: "1.2.1",
 };
 
 config.android = {
